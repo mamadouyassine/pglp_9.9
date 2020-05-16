78e4f147dcd0f4bddf111ca5d4bb99c53e6e97c3
uvsq21920982 <yassinemamadou97@gmail.com>
2020-05-16 20:58:02 +0200

    Classe forme

---
+++ src/main/java/fr/uvsq/pglp/Forme.java
@@ -0,0 +1,33 @@
+package fr.uvsq.pglp;
+
+public  abstract class Forme {
+/**
+ * Le nom de la forme.
+ */
+private String nom;
+/**
+ * Getter de nom.
+ * @return Le nom de la forme
+ */
+public String getNom() {
+        return nom;
+        }
+/**
+ * Setter de nom.
+ * @param n Le nouveau nom
+ */
+public void setNom(final String n) {
+        this.nom = n;
+        }
+/**
+ * Fonction de déplacement.
+ * @param x Ajout en abscisse par rapport a la position initiale
+ * @param y Ajout en ordonnée par rapport a la position initiale
+ */
+public abstract void move(int x, int y);
+/**
+ * Fonction d'affichage.
+ */
+public abstract void draw();
+
+}
