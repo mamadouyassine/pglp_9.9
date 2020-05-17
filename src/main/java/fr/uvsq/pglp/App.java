6d6059273a4365e9a13af30a20360046c0c78fc3
uvsq21920982 <yassinemamadou97@gmail.com>
2020-05-17 10:54:12 +0200

    All files

---
+++ src/main/java/fr/uvsq/pglp/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.pglp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
